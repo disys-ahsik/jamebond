8bcd6df18ff277fa0b1ffcb89920b3a2467a1560
disys-ahsik <95438581+disys-ahsik@users.noreply.github.com>
2021-12-13 11:07:48 +0530

    Add files via upload
    google pay code

---
+++ IMPIMPIMPTechnlogy.py
@@ -0,0 +1,58 @@
+ 
+1. Object Creation { Library creator  }
+     |                        |            |
+     Object's choice   Class     Method's
+
+2. Function call
+       |
+     Function Name  { Library Creator }
+
+3. We work with file system
+    PAss data of the file to Object , function
+
+4. Product   Process   Customer 
+     |               |             |
+    Database  Rest Api   Front end { html ,css }
+
+5. We dont use application to open file
+    We use library to work with filesystem
+
+6. What is Technology File System
+    Role :  Txt data  ==>  Python { data type }
+               |                      ||
+               harddisk         RAm
+
+7. Product   Process   Customer 
+     |               |             |
+    Database  Rest Api   Front end { html ,css }
+     MYSQL    Django     { html ,css }
+
+8. Database File
+
+   1.  To create file we need to code in MYsql or Python
+   2.  content of the data is stored in harddisk { application : mysql}
+   3.  Process of the data transfromation from database file to datastructure dict
+   
+    Name | Age | dob
+
+    jb       | 21  | 2-6-1998
+    cb       | 22  | 2-7-1998
+    cb       | 21  | 2-7-1998
+
+   [  { "Name":"jb","Age":21 ,"dob":"2-6-1998"  } ,  { "Name":"cb","Age":22 ,"dob":"2-7-1998"  } , {  }  ]
+
+9. command execute's code
+
+     pip       install          django
+       |           |                     |
+Exectuable command      install frameworks
+
+10. 
+
+
+
+
+   
+
+
+  
